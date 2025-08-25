ec8701dad289d2f8268c4e048f44d00234741a25
Jay kishan kharwar <jaykishankharwar215@gmail.com>
2025-08-25 11:53:35 +0530

    added localhost to cors options

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL, "http://localhost:300"],
+    origin: [process.env.CLIENT_URL, "http://localhost:3000"],
     credentials: true,
   })
 );
